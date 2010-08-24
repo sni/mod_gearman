c80a813027fd4a0e6617a61b429c50d41dda02fd
Sven Nierlein <sven@nierlein.de>
2010-08-24 17:48:30 +0200

    changed format of result

--- tools/devel/garbage.pl
+++ tools/devel/garbage.pl
@@ -137,7 +137,6 @@ _out("setting verbose level to ".$opt_verbose) if $opt_verbose >= 1;
 
 #################################################
 # Settings / General parameters
-my $i = 0;
 my $hostname = hostname(); # Can be used to confuse?
 
 #################################################
@@ -185,16 +184,7 @@ if (scalar @opt_hosts > 0){
 #################################################
 # Use Gearman::Client
 my $client = Gearman::Client->new;
-my $foo = new String::Random;
-
-#################################################
-# Parameters 
-my $tasks = $client->new_task_set;
-my $load = ""; # Will be filled later by randomness
-my $temp_hostname;
-my $temp_servicename;
-my $temp_checkcommand;
-my $temp_perfdata;
+my $rands  = new String::Random;
 
 #################################################
 # Set the targets for the junk
@@ -202,14 +192,20 @@ $client->job_servers(@opt_server);
 
 #################################################
 # Now insert junk
-for($i; $i < $opt_loops; $i++) {
-    $temp_hostname = _randomnessHost();
-    $temp_servicename = _randomnessService();
-    $temp_checkcommand = _randomcheckCommand();
-    $temp_perfdata = _randomperfData();
-    $load = "$temp_hostname $temp_servicename $temp_checkcommand \| $temp_perfdata";
-    my $handle = $tasks->add_task($opt_queue => $load);
-    _out("inserted $i out of $opt_loops, data: $load") if $opt_verbose >= 1; 
+my $now = time();
+my $end = $now+1;
+for(my $i = 0; $i < $opt_loops; $i++) {
+    my $temp_hostname     = _randomnessHost();
+    my $temp_servicename  = _randomnessService();
+    my $temp_output       = _randomOutput()."|"._randomperfData();
+    my $load;
+    if($i%2==0) {
+        $load             = "host_name=".$temp_hostname."\nservice_description=".$temp_servicename."\nstart_time=$now.0\nfinish_time=$end.0\noutput=".$temp_output."\n";
+    } else {
+        $load             = "host_name=".$temp_hostname."\nstart_time=$now.0\nfinish_time=$end.0\noutput=".$temp_output."\n";
+    }
+    my $handle         = $client->dispatch_background($opt_queue => $load);
+    _out("inserted $i out of $opt_loops, handle: $handle data: $load") if $opt_verbose >= 1; 
 }
 
 _out("finished inserting processing $opt_loops insertions of random junk into $opt_queue") if $opt_verbose >= 1;
@@ -230,7 +226,8 @@ sub _randomnessHost {
     if (scalar @opt_hosts > 0){
         $random_hostname = _randomselect(1);
     } else {
-        $random_hostname = $foo->randpattern("CCCCCnnc");
+        my $nr = int(rand(2000)); # generate random string with random size up to 200k chars
+        $random_hostname = $rands ->randregex(".{$nr}");
     }
     $random_hostname = $opt_hostprefix.$random_hostname if defined $opt_hostprefix;
     _out("randomness generated the following \"hostname\": $random_hostname") if $opt_verbose >= 3;
@@ -243,7 +240,8 @@ sub _randomnessService {
     if (scalar @opt_services > 0){
         $random_servicename = _randomselect(2);
     } else {
-        $random_servicename = $foo->randpattern("CCCcc");
+        my $nr = int(rand(2000)); # generate random string with random size up to 200k chars
+        $random_servicename = $rands ->randregex(".{$nr}");
     }
     $random_servicename = $opt_serviceprefix.$random_servicename if defined $opt_serviceprefix;
     _out("randomness generated the following \"service\": $random_servicename") if $opt_verbose >= 3;
@@ -252,22 +250,21 @@ sub _randomnessService {
 }
 
 #################################################
-sub _randomcheckCommand {
+sub _randomOutput {
     my $random_checkcommand;
-    $random_checkcommand = $foo->randpattern("cccccccccccccccccccccccccccCCCC");
+    $random_checkcommand = $rands ->randpattern("cccccccccccccccccccccccccccCCCC");
     _out("randomness generated the following \"check command\": $random_checkcommand") if $opt_verbose >= 3;
     return $random_checkcommand;
 }
 
 #################################################
 sub _randomperfData {
-    my $random_perfData;
-    my $min = $foo->randpattern("nnnn");
-    my $max = $foo->randpattern("nnnn");
-    my $warn = $foo->randpattern("nnnn");
-    my $crit = $foo->randpattern("nnnn");
-    my $value = $foo->randpattern("nnnn");
-    $random_perfData = "data=$value;$max;$min;$warn;$crit";
+    my $min   = $rands ->randpattern("nnnn");
+    my $max   = $rands ->randpattern("nnnn");
+    my $warn  = $rands ->randpattern("nnnn");
+    my $crit  = $rands ->randpattern("nnnn");
+    my $value = $rands ->randpattern("nnnn");
+    my $random_perfData = "data=$value;$max;$min;$warn;$crit";
     _out("randomness generated the following \"perfdata\": $random_perfData") if $opt_verbose >= 3;
     return $random_perfData;
 }
