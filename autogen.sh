8d366d642abbe618b4bfc75bfa886330512c4495
Sven Nierlein <sven@nierlein.de>
2010-09-09 00:13:03 +0200

    fixed dirs

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,5 @@
 #/bin/sh
 
-aclocal && autoheader && automake -a && autoconf
+aclocal && autoheader && automake -a && autoreconf --install --verbose
+
+echo " now run ./configure && make && make install"
