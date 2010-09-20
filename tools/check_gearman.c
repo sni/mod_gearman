943c986ffe5dad321c9caa1337d8208f816326dd
Sven Nierlein <sven@nierlein.de>
2010-09-20 14:54:23 +0200

    added missing port in getop

--- tools/check_gearman.c
+++ tools/check_gearman.c
@@ -48,7 +48,7 @@ int main (int argc, char **argv) {
      */
 
     int opt;
-    while((opt = getopt(argc, argv, "hvH:t:w:c:q:s:e:")) != -1) {
+    while((opt = getopt(argc, argv, "hvH:t:w:c:q:s:e:p:")) != -1) {
         switch(opt) {
             case 'h':   print_usage();
                         exit( STATE_UNKNOWN );
