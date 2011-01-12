c6d1a06b2e8c9be6990f48b719534030b999cfeb
Sven Nierlein <sven@nierlein.de>
2011-01-12 09:56:20 +0100

    increased max output to 64k

--- include/common.h
+++ include/common.h
@@ -31,8 +31,8 @@
 #define GM_VERSION                  "0.8"
 #define GM_ENABLED                      1
 #define GM_DISABLED                     0
-#define GM_BUFFERSIZE               16384
-#define GM_MAX_OUTPUT               11000   /* must be ~30% below GM_BUFFERSIZE for base64/encryption */
+#define GM_BUFFERSIZE               98304
+#define GM_MAX_OUTPUT               65536   /* must be ~30% below GM_BUFFERSIZE for base64/encryption */
 #define GM_LISTSIZE                   512
 
 #define GM_MIN_LIB_GEARMAN_VERSION   0.14
