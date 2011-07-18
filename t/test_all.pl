8a45433644a72e9d0ebbd9c83b6806763a57a38f
Sven Nierlein <sven@nierlein.de>
2011-07-18 16:13:47 +0200

    nicer tests

--- t/test_all.pl
+++ t/test_all.pl
@@ -13,16 +13,18 @@ plan(tests => 13);
 `make`;
 is($?, 0, "build rc is $?");
 
-my $vallog = '/tmp/valgrind.log';
+my $vallog  = '/tmp/valgrind.log';
+my $testlog = '/tmp/mod_gearman_test.log';
 for my $test (qw(01_utils 02_full 03_exec 04_log)) {
     `make $test`;
     is($?, 0, "$test build rc is $?");
 
-    `valgrind --tool=memcheck --leak-check=yes --leak-check=full --show-reachable=yes --track-origins=yes --log-file=/tmp/valgrind.log ./$test >/dev/null`;
-    is($?, 0, "$test valgrind exit code is $?");
+    `valgrind --tool=memcheck --leak-check=yes --leak-check=full --show-reachable=yes --track-origins=yes --log-file=/tmp/valgrind.log ./$test >$testlog 2>&1`;
+    is($?, 0, "$test valgrind exit code is $?") or diag(`cat $testlog`);
 
     is(qx(grep "ERROR SUMMARY: " $vallog | grep -v "ERROR SUMMARY: 0 errors"), "", "valgrind Error Summary")
       or BAIL_OUT("check memory $test in $vallog");
 }
 
 unlink($vallog);
+unlink($testlog);
