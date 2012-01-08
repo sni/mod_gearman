231aac68d5d58016a50c3a77088d30aa3f1d7f44
Sven Nierlein <sven@nierlein.de>
2012-01-08 20:02:13 +0100

    release preperations

--- include/common.h
+++ include/common.h
@@ -53,7 +53,7 @@
 #define MOD_GM_COMMON_H
 
 /* constants */
-#define GM_VERSION                 "1.1.2"
+#define GM_VERSION                 "1.2.0"
 #define GM_ENABLED                      1
 #define GM_DISABLED                     0
 #define GM_BUFFERSIZE               98304
