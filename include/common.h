666b3a013f2f30320983cc3f90a22d33707197db
Sven Nierlein <Sven.Nierlein@consol.de>
2013-07-27 13:44:42 +0200

    released 1.4.8

--- include/common.h
+++ include/common.h
@@ -54,7 +54,7 @@
 #define MOD_GM_COMMON_H
 
 /* constants */
-#define GM_VERSION                 "1.4.6"
+#define GM_VERSION                 "1.4.8"
 #define GM_ENABLED                      1
 #define GM_DISABLED                     0
 #define GM_BUFFERSIZE               98304
