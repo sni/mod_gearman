b6e4a41f78a5cf0f030a8fdb894eab1bfaca47c1
Sven Nierlein <Sven.Nierlein@consol.de>
2015-07-01 13:58:22 +0200

    release 2.1.3

--- include/common.h
+++ include/common.h
@@ -55,7 +55,7 @@
 #define MOD_GM_COMMON_H
 
 /* constants */
-#define GM_VERSION                 "2.1.2"
+#define GM_VERSION                 "2.1.3"
 #define GM_ENABLED                      1
 #define GM_DISABLED                     0
 #define GM_BUFFERSIZE               65536
