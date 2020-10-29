3b7184ca47bfc6b1af82310085f566e90b3fc5d7
A Codeweavers Infrastructure Bod <36475663+infraweavers@users.noreply.github.com>
2020-10-29 16:36:18 +0000

    Switching back to the default size

--- include/common.h
+++ include/common.h
@@ -58,7 +58,7 @@
 #define GM_VERSION                 "3.3.3"
 #define GM_ENABLED                      1
 #define GM_DISABLED                     0
-#define GM_BUFFERSIZE            10485760
+#define GM_BUFFERSIZE               65536
 #define GM_SMALLBUFSIZE              1024
 #define GM_MAX_OUTPUT            10485760   /* limit plugin output size to 10mb */
 #define GM_LISTSIZE                   512
