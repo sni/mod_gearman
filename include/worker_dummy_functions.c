5e7162a5c80e3875c516894156433b7c2d5fffd4
Sven Nierlein <sven@consol.de>
2024-11-06 16:54:58 +0100

    add missing prototype

--- include/worker_dummy_functions.c
+++ include/worker_dummy_functions.c
@@ -10,6 +10,7 @@ char * eventtype2str(__attribute__((__unused__)) int i) {
     return strdup("UNKNOWN");
 }
 
+void nm_log(__attribute__((__unused__)) int, __attribute__((__unused__))const char *, ...);
 void nm_log(__attribute__((__unused__)) int, __attribute__((__unused__))const char *, ...) {
     return;
 }
