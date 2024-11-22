45e65d15466f0ee1c00280fe195302b122e4304d
Sven Nierlein <Sven.Nierlein@consol.de>
2024-11-22 08:55:12 +0100

    release v5.1.7

--- include/common.h
+++ include/common.h
@@ -55,7 +55,7 @@
 #define MOD_GM_COMMON_H
 
 /* constants */
-#define GM_VERSION                 "5.1.6"
+#define GM_VERSION                 "5.1.7"
 #define GM_ENABLED                      1
 #define GM_DISABLED                     0
 #define GM_AUTO                         2
