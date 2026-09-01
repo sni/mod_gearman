3cc957ab92d58ca9d6e72945264062ec3e026a98
nook24 <info@nook24.eu>
2026-09-01 20:25:48 +0200

    Make sure the async client exists and will be freed
    Signed-off-by: nook24 <info@nook24.eu>

--- common/gearman_utils.c
+++ common/gearman_utils.c
@@ -320,7 +320,9 @@ submit_done:
         }
 
         /* recreate client, otherwise gearman sigsegvs */
-        gm_release_pending();
+        if(async) {
+            gm_release_pending();
+        }
         gm_free_client(client);
         if(async) {
             *client = create_client(server_list);
