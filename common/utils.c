b5dcfe2c40624a7ea106f1c848c4aee29550e494
nook24 <info@nook24.eu>
2026-09-01 20:08:47 +0200

    Create a copy of the original va_list using va_copy as reusing the same va_list is undefined behavior and can produce corrupted output or crashes on some platforms
    Signed-off-by: nook24 <info@nook24.eu>

--- common/utils.c
+++ common/utils.c
@@ -1543,6 +1543,7 @@ static void gm_log_body( int lvl, const char *text, va_list ap_in ) {
     struct timeval tv;
     struct tm now;
     bool locked = false;
+    va_list ap_copy;
 
     if(mod_gm_opt != NULL) {
         debug_level = mod_gm_opt->debug_level;
@@ -1560,7 +1561,9 @@ static void gm_log_body( int lvl, const char *text, va_list ap_in ) {
         } else {
             snprintf( buffer1, 14, "mod_gearman: " );
         }
-        vsnprintf( buffer1 + strlen( buffer1 ), sizeof( buffer1 ) - strlen( buffer1 ), text, ap_in );
+        va_copy(ap_copy, ap_in);
+        vsnprintf( buffer1 + strlen( buffer1 ), sizeof( buffer1 ) - strlen( buffer1 ), text, ap_copy );
+        va_end(ap_copy);
 
         if ( debug_level >= GM_LOG_STDOUT ) {
             printf( "%s", buffer1 );
@@ -1603,7 +1606,9 @@ static void gm_log_body( int lvl, const char *text, va_list ap_in ) {
     /* append milliseconds and pid */
     snprintf(buffer2, sizeof(buffer2), ",%03ld][%i][%s]", tv.tv_usec/1000, getpid(), level);
 
-    vsnprintf( buffer3, GM_BUFFERSIZE, text, ap_in );
+    va_copy(ap_copy, ap_in);
+    vsnprintf( buffer3, GM_BUFFERSIZE, text, ap_copy );
+    va_end(ap_copy);
 
     if ( debug_level >= GM_LOG_STDOUT || logmode == GM_LOG_MODE_CHECKS ) {
         fprintf(stderr, "%s", buffer3 );
